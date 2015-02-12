fd6c850cc1d7639b7e5da952a5fcbac718f45e98
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-02-12 23:37:25 +0000

    Basic folder filtering by prefix.

--- explorer/explorer.c
+++ explorer/explorer.c
@@ -10,6 +10,18 @@ char search[256];
 int cdir;
 int steps=0;
 
+const char* exclude_prefix[] = {"//proc", "//dev", "//usr/lib", "//usr/lib64", "//usr/share", "."};
+
+int check_dir(char *dir){
+   int l = strlen(dir), i;
+   for(i=0; i<sizeof(exclude_prefix)/sizeof(char*); i++){
+      if(strlen(exclude_prefix[i]) <= l && strncmp(dir, exclude_prefix[i], l) == 0)
+         return false;
+
+   }
+   return true;
+}
+
 void dfs(char* dir_name, int depth){
    if(depth > cdir){
       return;
@@ -24,7 +36,7 @@ void dfs(char* dir_name, int depth){
       if(strcmp(file->d_name, "..") != 0 && strcmp(file->d_name, ".") != 0){
          sprintf(name, "%s/%s", dir_name, file->d_name);
          stat(name, &mystat);
-         if(S_ISDIR(mystat.st_mode)){
+         if(S_ISDIR(mystat.st_mode) && check_dir(name)){
             dfs(name, depth+1);
          }else{
             if(strstr(file->d_name, search) != NULL && cdir == depth){
