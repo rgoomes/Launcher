5c4d3b4e05969a93787ed4d63d68f48c1277d42d
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-02-12 23:03:26 +0000

    Removed debugs

--- explorer/explorer.c
+++ explorer/explorer.c
@@ -11,7 +11,6 @@ int cdir;
 int steps=0;
 
 void dfs(char* dir_name, int depth){
-   //printf("%d\n", steps++);
    if(depth > cdir){
       return;
    }
@@ -26,7 +25,6 @@ void dfs(char* dir_name, int depth){
          sprintf(name, "%s/%s", dir_name, file->d_name);
          stat(name, &mystat);
          if(S_ISDIR(mystat.st_mode)){
-            //printf("%s\n", myfile->d_name);
             dfs(name, depth+1);
          }else{
             if(strstr(file->d_name, search) != NULL && cdir == depth){
@@ -42,30 +40,9 @@ void dfs(char* dir_name, int depth){
 
 int main(int argc, char* argv[])
 {
-   /*DIR *mydir;
-   struct dirent *myfile;
-   struct stat mystat;
-
-   char name[100];
-   mydir = opendir(argv[1]);
-   printf("dir: %s\n", argv[1]);
-   while((myfile = readdir(mydir)) != NULL){
-
-      sprintf(name, "%s/%s", argv[1], myfile->d_name);
-      stat(name, &mystat);
-      printf("%d",mystat.st_size);
-      printf(" %s %d\n", myfile->d_name, S_ISDIR(mystat.st_mode) );
-   }
-   closedir(mydir);*/
-
-   /*strcpy(queue[qsize], argv[1]);
-   qsize++;
    strcpy(search, argv[2]);
-   bfs();*/
-
-   strcpy(search, argv[2]);
-   for(cdir=1; cdir<=10; cdir++){
-      printf("cdir: %d\n", cdir);
+   for(cdir=0; cdir<=10; cdir++){
+      //printf("cdir: %d\n", cdir);
       dfs(argv[1], 0);
    }
    printf("END\n");
